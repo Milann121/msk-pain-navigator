e24d94980af02e1e01ffc0a69699e319485effaf
Milann121 <milan@duda.sk>
2025-06-26 17:13:00 +0200

    commit

---
+++ src/data/UpperLimb/Advices/advicesDatabase.ts
@@ -0,0 +1,23 @@
+export interface Advice {
+  adviceId: number;
+  adviceTitle: string;
+  adviceSubtitle: string;
+  advicePriority: string;
+  adviceRule: string;
+  adviceDescription: string;
+  adviceImageUrl: string;
+  adviceLink: string;
+}
+
+export const advices: Advice[] = [
+  {
+    adviceId: 001,
+    adviceTitle: "Pravidelná zmena polohy",
+    adviceSubtitle: " ",
+    advicePriority: "Vysoká",
+    adviceRule: "Každých 45-60min."
+    adviceDescription: "Vaše bolesti môžu byť spôsobované aj statickou záťažou, ktorá pri vašom probléme nie je vhodná. Snažte sa pravidelné meniť polohy, čo vám zabezpečí potrebnú úľavu od bolesti a zníženie rizika ďalších bolestí.",
+    adviceImageUrl: "https://via.placeholder.com/150",
+    adviceLink: "https://www.google.com",
+  },
+];
